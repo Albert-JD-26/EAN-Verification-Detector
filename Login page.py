8a4824a5c944efa93e169dcb861cb5efa68ec329
Albert  J <150644012+Albert-JD-26@users.noreply.github.com>
2023-11-29 22:04:50 +0530

    Update Login page.py

--- Login page.py
+++ Login page.py
@@ -8,6 +8,13 @@ import pyautogui
 from io import BytesIO
 import pandas as pd
 import os
+import cv2
+
+try:
+    if not os.path.exists('DATABASES'):
+        os.makedirs('DATABASES')
+except OSError:
+    print('Error: Creating directory of DATABASES')
 
 
 def get_display_resolution():
@@ -46,7 +53,85 @@ def check_company_name_not_null():
     return len(result) > 0
 
 
+def record():
+    # Detect the number of camera connected to system #
+    camera_list = [f'Camera {i}' for i in range(10) if cv2.VideoCapture(i).read()[0]]
+    print(f'Detected cameras: {", ".join(camera_list)}')
+    Number_of_camera = len(camera_list)
+    print("Total number of cameras:", Number_of_camera)
+
+    if Number_of_camera == 1:
+        print("One camera is connected to system")
+
+
+def display_summary():
+    frame = tk.Frame(main_frame)
+    frame.pack(fill=tk.BOTH, expand=True)
+
+    canvas = tk.Canvas(frame)
+    canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+    output_frame = tk.Frame(canvas)
+    canvas.create_window((0, 0), window=output_frame, anchor=tk.NW)
+
+    p1 = tk.Label(output_frame, text="VIDEO NAME", font="time 15 bold")
+    p1.grid(row=1, column=0, padx=10, pady=10)
+
+    p4 = tk.Label(output_frame, text="DURATION", font="time 15 bold")
+    p4.grid(row=1, column=1, padx=10, pady=10)
+
+    p2 = tk.Label(output_frame, text="NO OF DATA", font="time 15 bold")
+    p2.grid(row=1, column=2, padx=10, pady=10)
+
+    p3 = tk.Label(output_frame, text="DATE", font="time 15 bold")
+    p3.grid(row=1, column=3, padx=10, pady=10)
+
+    p5 = tk.Label(output_frame, text="TIME", font="time 15 bold")
+    p5.grid(row=1, column=4, padx=10, pady=10)
+
+    p6 = tk.Label(output_frame, text="USERNAME", font="time 15 bold")
+    p6.grid(row=1, column=5, padx=10, pady=10)
+
+    conn = sqlite3.connect("DATABASES/recording.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM recordsummary")
+    r = c.fetchall()
+
+    num = 2
+    for i in r:
+        video_name = tk.Label(output_frame, text=i[0], font="time 11 bold", fg="black")
+        video_name.grid(row=num, column=0, padx=10, pady=5)
+
+        duration = tk.Label(output_frame, text=i[1], font="time 11 bold", fg="blue")
+        duration.grid(row=num, column=1, padx=10, pady=5)
+
+        no_od_data = tk.Label(output_frame, text=i[2], font="time 11 bold", fg="red")
+        no_od_data.grid(row=num, column=2, padx=10, pady=5)
+
+        date = tk.Label(output_frame, text=i[3], font="time 11 bold", fg="green")
+        date.grid(row=num, column=3, padx=10, pady=5)
+
+        time = tk.Label(output_frame, text=i[4], font="time 11 bold", fg="pink")
+        time.grid(row=num, column=4, padx=10, pady=5)
+
+        username = tk.Label(output_frame, text=i[5], font="time 11 bold", fg="grey")
+        username.grid(row=num, column=5, padx=10, pady=5)
+
+        num = num + 1
+
+    output_frame.update_idletasks()
+    canvas.configure(scrollregion=canvas.bbox("all"))
+    canvas.bind_all("<MouseWheel>",  lambda event: canvas.yview_scroll(int(-1 * (event.delta / 120)), "units"))
+
+
 def export_summary():
+
     conn = sqlite3.connect('DATABASES/recording.db')
     df = pd.read_sql_query("SELECT * from recordsummary", conn)
     df.to_excel(f"RECORDING SUMMARY.xlsx", index=False)
@@ -55,7 +140,7 @@ def export_summary():
     messagebox.showinfo("Exported", "Overall data exported to Excel successfully.")
 
 
-# With user credentials user can logged into the software ↓ #
+# With user credentials user can log into the software ↓ #
 
 
 def login_account():
@@ -88,53 +173,53 @@ def login_account():
         connection.close()
         return result and result[0] == 'employee'
 
-    def admin_portal(u_name, password):
+    def create_user():
+        admin_panel.destroy()
+
+        def create_user_key():
+            users_name = name_user.get()
+            users_password = password_set.get()
+
+            connection = sqlite3.connect('EAN.db')
+            u_connection = connection.cursor()
+            u_connection.execute("INSERT INTO users (user_name,designation, password) VALUES (?, 'employee', ?)",
+                                 (users_name, users_password))
+            connection.commit()
+            u_connection.close()
+            connection.close()
+            c_user.destroy()
+            admin_portal_1920x1080(u_name, password)
+
+        c_user = Tk()
+        c_user.geometry('600x400')
+        c_user.title('Create User')
+
+        user_name = Label(c_user, text='User Name')
+        user_name.pack()
+
+        name_user = Entry(c_user)
+        name_user.pack()
+
+        set_password = Label(c_user, text='Password')
+        set_password.pack()
+
+        password_set = Entry(c_user)
+        password_set.pack()
+
+        create_button = Button(c_user, text='CREATE', command=create_user_key)
+        create_button.pack()
+
+        c_user.mainloop()
+
+    def admin_portal_1920x1080(u_name, password):
 
         # In this function by if condition users can be classified whether admin or employee #
         # If condition for admin #
         if is_admin(u_name, password):
             print('Admin Panel')
-
-            def create_user():
-                admin_panel.destroy()
-
-                def create_user_key():
-                    users_name = name_user.get()
-                    users_password = password_set.get()
-
-                    connection = sqlite3.connect('EAN.db')
-                    u_connection = connection.cursor()
-                    u_connection.execute("INSERT INTO users (user_name,designation, password) VALUES (?, 'employee', ?)",
-                                         (users_name, users_password))
-                    connection.commit()
-                    u_connection.close()
-                    connection.close()
-                    c_user.destroy()
-                    admin_portal(u_name, password)
-
-                c_user = Tk()
-                c_user.geometry('600x400')
-                c_user.title('Create User')
-
-                user_name = Label(c_user, text='User Name')
-                user_name.pack()
-
-                name_user = Entry(c_user)
-                name_user.pack()
-
-                set_password = Label(c_user, text='Password')
-                set_password.pack()
-
-                password_set = Entry(c_user)
-                password_set.pack()
-
-                create_button = Button(c_user, text='CREATE', command=create_user_key)
-                create_button.pack()
-
-                c_user.mainloop()
-
+            global admin_panel
             admin_panel = Tk()
-            admin_panel.geometry(f"{display_resolution[0]}x{display_resolution[1]}")
+            admin_panel.geometry("1920x1080")
             admin_panel.title('ADMIN PANEL')
 
             options_frame = tk.Frame(admin_panel, bg="#c3c3c3", highlightbackground='black', highlightthickness=2)
@@ -146,10 +231,10 @@ def login_account():
             general = tk.Label(options_frame, text="GENERAL", font=10, bg="#c3c3c3")
             general.place(x=50, y=20)
 
-            rec_btn = tk.Button(options_frame, text="RECORD", font=('Bold', 15), fg='white', bd=0, bg='#4392fa',width=15,borderwidth=5)
+            rec_btn = tk.Button(options_frame, text="RECORD", font=('Bold', 15), fg='white', bd=0, bg='#4392fa',width=15,borderwidth=5, command=record)
             rec_btn.place(x=10, y=50)
 
-            summary_btn = tk.Button(options_frame, text="DISPLAY \n SUMMARY", font=('Bold', 15), fg='white', bd=0,bg='#4392fa',width=15, borderwidth=5)
+            summary_btn = tk.Button(options_frame, text="DISPLAY \n SUMMARY", font=('Bold', 15), fg='white', bd=0,bg='#4392fa',width=15, borderwidth=5, command=display_summary)
             summary_btn.place(x=10, y=110)
 
             exp_rec_summary_bt = tk.Button(options_frame, text="EXPORT REC\n SUMMARY", font=('Bold', 15),fg='white', bd=0,bg='#4392fa',width=15, borderwidth=5, command=export_summary)
@@ -177,6 +262,130 @@ def login_account():
             delete_db.place(x=60, y=750)
 
             # MAIN FRAME
+            global main_frame
+            main_frame = tk.Frame(admin_panel, highlightbackground='black', highlightthickness=2)
+            main_frame.pack(side=tk.LEFT)
+            main_frame.pack_propagate(False)
+            main_frame.configure(height=1500, width=7100, bg='#2c2f33')
+
+            admin_panel.mainloop()
+
+        # Elif condition for employee #
+        elif is_user(u_name, password):
+            print('User Panel')
+
+            user_panel = Tk()
+            user_panel.geometry("1920x1080")
+            user_panel.title('USER PANEL')
+            connect = sqlite3.connect('EAN.db')
+            to_connect_name = connect.cursor()
+            to_connect_name.execute("SELECT Images FROM details")
+            image_data = to_connect_name.fetchone()[0]
+
+            image_stream = BytesIO(image_data)
+            original_img = Image.open(image_stream)
+            desired_width = 80
+            desired_height = 80
+            resized_img = original_img.resize((desired_width, desired_height), Image.LANCZOS)
+            img = ImageTk.PhotoImage(resized_img)
+
+            # OPTION FRAME
+            options_frame = tk.Frame(user_panel, bg="#c3c3c3", highlightbackground='black', highlightthickness=2)
+            options_frame.pack(side=tk.LEFT)
+            options_frame.pack_propagate(False)
+            options_frame.configure(width=210, height=1500)
+
+            # BUTTONS
+            general = tk.Label(options_frame, text="GENERAL", font=10, bg="#c3c3c3")
+            general.place(x=50, y=150)
+
+            rec_btn = tk.Button(options_frame, text="RECORD", font=('Bold', 15), fg='white', bd=0, bg='#4392fa', width=15, borderwidth=5)
+            rec_btn.place(x=10, y=200)
+
+            summary_btn = tk.Button(options_frame, text="DISPLAY \n SUMMARY", font=('Bold', 15), fg='white', bd=0, bg='#4392fa', width=15, borderwidth=5)
+            summary_btn.place(x=10, y=270)
+
+            exp_rec_summary_bt = tk.Button(options_frame, text="EXPORT REC\n SUMMARY", font=('Bold', 15), fg='white', bd=0, bg='#4392fa', width=15, borderwidth=5, command=export_summary)
+            exp_rec_summary_bt.place(x=10, y=370)
+
+            label = Label(options_frame, image=img)
+            label.place(x=58, y=800)
+
+            company_name_display = Label(options_frame, text=f'{comp_name}', font=('Kozuka Gothic Pro B', 10, 'italic'), bg="#c3c3c3")
+            company_name_display.place(x=70, y=900)
+
+            company_location = Label(options_frame, text=f'{location_company}', font=('Kozuka Gothic Pro B', 10, 'italic'), bg="#c3c3c3")
+            company_location.place(x=70, y=920)
+
+            # MAIN FRAME
+            main_frame = tk.Frame(user_panel, highlightbackground='black', highlightthickness=2)
+            main_frame.pack(side=tk.LEFT)
+            main_frame.pack_propagate(False)
+            main_frame.configure(height=1500, width=7100, bg='#2c2f33')
+
+            user_panel.mainloop()
+
+        else:
+            print('Invalid credentials or you do not have access to the admin portal.')
+            messagebox.showerror('Invalid Credentials', f'Username or password is wrong')
+
+    def admin_portal_1280x720(u_name, password):
+
+        # In this function by if condition users can be classified whether admin or employee #
+        # If condition for admin #
+        if is_admin(u_name, password):
+            print('Admin Panel')
+            global admin_panel
+
+            admin_panel = Tk()
+            admin_panel.geometry("1280x720")
+            admin_panel.title('ADMIN PANEL')
+
+            options_frame = tk.Frame(admin_panel, bg="#c3c3c3", highlightbackground='black', highlightthickness=2)
+            options_frame.pack(side=tk.LEFT)
+            options_frame.pack_propagate(False)
+            options_frame.configure(width=210, height=1500)
+
+            # BUTTONS
+            general = tk.Label(options_frame, text="GENERAL", font=10, bg="#c3c3c3")
+            general.place(x=50, y=15)
+
+            rec_btn = tk.Button(options_frame, text="RECORD", font=('Bold', 13), fg='white', bd=0, bg='#4392fa',
+                                width=15, borderwidth=5)
+            rec_btn.place(x=23, y=45)
+
+            summary_btn = tk.Button(options_frame, text="DISPLAY \n SUMMARY", font=('Bold', 13), fg='white', bd=0,
+                                    bg='#4392fa', width=15, borderwidth=5, command=display_summary)
+            summary_btn.place(x=23, y=100)
+
+            exp_rec_summary_bt = tk.Button(options_frame, text="EXPORT REC\n SUMMARY", font=('Bold', 13), fg='white',
+                                           bd=0, bg='#4392fa', width=15, borderwidth=5, command=export_summary)
+            exp_rec_summary_bt.place(x=23, y=175)
+
+            options = tk.Label(options_frame, text="OPTIONS", font=15, bg="#c3c3c3")
+            options.place(x=50, y=245)
+
+            input_btn = tk.Button(options_frame, text="INPUT \n VIDEO", font=('Bold', 13), fg='white', bd=0,
+                                  bg='#4392fa', width=15, borderwidth=5)
+            input_btn.place(x=23, y=280)
+
+            data_btn = tk.Button(options_frame, text="EXPORT\nCAPTURED\nDATA", font=('Bold', 13), fg='white', bd=0,
+                                 bg='#4392fa', width=15, borderwidth=5)
+            data_btn.place(x=23, y=360)
+
+            exp_input_summary_bt = tk.Button(options_frame, text="EXPORT INPUT\n SUMMARY", font=('Bold', 13),
+                                             fg='white', bd=0, bg='#4392fa', width=15, borderwidth=5)
+            exp_input_summary_bt.place(x=23, y=465)
+
+            download_btn = tk.Button(options_frame, text="DOWNLOAD \n CLIP", font=('Bold', 13), fg='white', bd=0,
+                                     bg='#4392fa', width=15, borderwidth=5)
+            download_btn.place(x=23, y=550)
+
+            cre_user = Button(options_frame, text='CREATE USER', font=('Bold', 13), fg='white', bd=0, bg='#4392fa',
+                              width=15, borderwidth=5)
+            cre_user.place(x=23, y=635)
+
+            # MAIN FRAME
 
             main_frame = tk.Frame(admin_panel, highlightbackground='black', highlightthickness=2)
             main_frame.pack(side=tk.LEFT)
@@ -190,7 +399,7 @@ def login_account():
             print('User Panel')
 
             user_panel = Tk()
-            user_panel.geometry(f"{display_resolution[0]}x{display_resolution[1]}")
+            user_panel.geometry("1280x720")
             user_panel.title('USER PANEL')
             connect = sqlite3.connect('EAN.db')
             to_connect_name = connect.cursor()
@@ -249,12 +458,12 @@ def login_account():
     if check_user_name_not_null():
         if display_resolution[0] == 1920 and display_resolution[1] == 1080:
             log.destroy()
-            admin_portal(u_name, password)
+            admin_portal_1920x1080(u_name, password)
             print('laptop')
 
-        elif display_resolution[0] == 800 and display_resolution[1] == 600:
+        elif display_resolution[0] == 1280 and display_resolution[1] == 720:
             log.destroy()
-            admin_portal(u_name, password)
+            admin_portal_1280x720(u_name, password)
             print('pc')
 
     else:
@@ -307,6 +516,7 @@ def login_account():
 
 # Login box is created with company name. ↓ #
 
+
 def login():
 
     global log
@@ -330,7 +540,7 @@ def login():
 
     log = Tk()
     log.geometry("900x430")
-    log.resizable(0, 0)
+    log.resizable(0,0)
     log.config(bg='#f0f0f0')
     log.title(f"{comp_name} LOGIN")
 
@@ -483,5 +693,3 @@ else:
     submit.place(x=250, y=310)
 
     w.mainloop()
-
-
